a6fa437eafe81fe7fd7dde5f51707f1496e4e79b
yoghantara08 <gyoghantara@gmail.com>
2025-05-05 17:22:57 +0800

    refactor: update Button component variants and styles; adjust global theme colors

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,7 +9,7 @@ export interface ButtonProps {
   disabled?: boolean;
   // eslint-disable-next-line no-unused-vars
   onClick?: (() => void) | ((e: any) => void);
-  variant?: "primary" | "secondary";
+  variant?: "primary" | "secondary" | "tertiary" | "ghost";
   type?: "button" | "submit" | "reset";
 }
 
@@ -29,16 +29,25 @@ const Button = ({
       onClick={!disabled ? onClick : undefined}
       disabled={disabled}
       className={classNames(
-        "inline-flex items-center justify-center rounded-lg px-4 py-3",
-        "text-sm font-medium transition-colors xl:text-base",
+        "m-2 inline-flex items-center justify-center rounded-3xl px-4 py-3",
+        "text-sm font-semibold transition-colors",
         "focus:outline-none",
         {
           // Primary variant
-          "bg-accent hover:bg-accent/80": variant === "primary" && !disabled,
+          "text-background bg-accent hover:bg-accent/80":
+            variant === "primary" && !disabled,
 
           // Secondary variant
-          "hover:bg-accent/30 border-accent/30 text-accent border":
+          "border-accent/30 text-light-accent hover:bg-accent/30 border":
             variant === "secondary" && !disabled,
+
+          // Tertiary variant
+          "hover:border-accent/40 text-light-accent bg-accent/30 border border-transparent":
+            variant === "tertiary" && !disabled,
+
+          // Ghost variant
+          "text-accent hover:border-accent/40 hover:bg-accent/10 border border-transparent bg-transparent":
+            variant === "ghost" && !disabled,
 
           "bg-accent/60 text-muted cursor-not-allowed": disabled,
         },
