4c71c60aca55a6ee1fcee0ab14960bfbcaee42ba
yoghantara08 <gyoghantara@gmail.com>
2025-09-21 22:48:55 +0800

    adjust dashboard menu

--- src/features/dashboard/DashboardTabs/DashboardTabs.tsx
+++ src/features/dashboard/DashboardTabs/DashboardTabs.tsx
@@ -1,60 +1,41 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 
-import { TabPanel } from "@headlessui/react";
 import { useAccount } from "wagmi";
 
-import TabMenu from "@/components/TabMenu/TabMenu";
 import CustomConnectButton from "@/components/Web3Provider/CustomConnectButton";
 
 import BorrowedPositions from "./BorrowedPositions";
 import SuppliedPositions from "./SuppliedPositions";
-import TransactionsHistory from "./TransactionsHistory";
 
-const DashboardTabsMenu = [{ label: "Portfolio" }, { label: "Transactions" }];
+// const DashboardTabsMenu = [{ label: "Portfolio" }, { label: "Transactions" }];
 
 const DashboardTabs = () => {
   const { isConnected } = useAccount();
 
-  const [menu, setMenu] = useState(0);
+  // const [menu, setMenu] = useState(0);
 
   return (
     <div className="">
-      <TabMenu
-        tabs={DashboardTabsMenu.map((v) => v.label)}
-        onChange={setMenu}
-        currentIndex={menu}
-        tabListClassName="sm:w-[500px] w-full"
-        tabPanelsClassName="py-6"
-        tabItemClassName="!py-3 md:!py-5"
-      >
-        {isConnected ? (
-          <TabPanel as="div" className="grid gap-5 xl:grid-cols-2">
-            <SuppliedPositions />
-            <BorrowedPositions />
-          </TabPanel>
-        ) : (
-          <TabPanel
-            as="div"
-            className="bg-surface border-elevated flex h-[270px] w-full items-center justify-center rounded-xl border sm:h-[500px]"
-          >
-            <div className="flex flex-col items-center gap-1 text-center">
-              <p className="text-lg font-semibold sm:text-2xl">
-                Please, connect your wallet
-              </p>
-              <p className="text-tertiary mb-2 text-sm font-medium sm:text-base">
-                Connect your wallet to see your supplies, borrowings, and open
-                positions.
-              </p>
-              <CustomConnectButton />
-            </div>
-          </TabPanel>
-        )}
-
-        <TabPanel>
-          <TransactionsHistory />
-        </TabPanel>
-      </TabMenu>
+      {isConnected ? (
+        <div className="grid gap-5 xl:grid-cols-2">
+          <SuppliedPositions />
+          <BorrowedPositions />
+        </div>
+      ) : (
+        <div className="bg-surface border-elevated flex h-[270px] w-full items-center justify-center rounded-xl border sm:h-[500px]">
+          <div className="flex flex-col items-center gap-1 text-center">
+            <p className="text-lg font-semibold sm:text-2xl">
+              Please, connect your wallet
+            </p>
+            <p className="text-tertiary mb-2 text-sm font-medium sm:text-base">
+              Connect your wallet to see your supplies, borrowings, and open
+              positions.
+            </p>
+            <CustomConnectButton />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
